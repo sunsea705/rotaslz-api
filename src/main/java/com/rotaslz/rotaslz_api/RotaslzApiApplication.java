73a03c061343cf8b675ab6bb2e05f78fac07942e
Carolina Souza <carolinasouza@tce.ma.gov.br>
2024-11-27 12:50:48 -0300

    commit inicial

---
+++ src/main/java/com/rotaslz/rotaslz_api/RotaslzApiApplication.java
@@ -0,0 +1,13 @@
+package com.rotaslz.rotaslz_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaslzApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaslzApiApplication.class, args);
+	}
+
+}
