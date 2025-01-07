f1104780dffcb1fe1334f485d54679e23a09c583
Carolina Souza <carolinasouza@tce.ma.gov.br>
2025-01-07 10:05:49 -0300

    nice

---
+++ src/main/java/com/rotaslzapi/entities/LinhaLocalidade.java
@@ -0,0 +1,35 @@
+package com.rotaslzapi.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "linha_localidade")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinhaLocalidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "linha_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Linha linha;
+
+    @ManyToOne
+    @JoinColumn(name = "localidade_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Localidade localidade;
+
+    private int ordem;
+
+}
